c6071875b08a23326b323d26875bb4f7962d9719
Marcus Koh <kohchihao@gmail.com>
2023-10-04 11:31:27 +0800

    fix: formatting on ios

--- ios/NativeModulesPractice.mm
+++ ios/NativeModulesPractice.mm
@@ -3,35 +3,30 @@
 @interface RCT_EXTERN_MODULE(NativeModulesPractice, NSObject)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(plus:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(simpleMethodReturns: (RCTResponseSenderBlock) callback)
 
 RCT_EXTERN_METHOD(simpleMethodWithParams:(NSString *) param
-    callback: (RCTResponseSenderBlock)callback
-  )
+                  callback: (RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(simpleMethodWithObj:(NSDictionary<NSString *, id> *)param callback:(RCTResponseSenderBlock)callback
-  )
+RCT_EXTERN_METHOD(simpleMethodWithObj:(NSDictionary<NSString *, id> *)param
+                  callback:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(
-    resolvePromise: (RCTPromiseResolveBlock) resolve
-    rejecter: (RCTPromiseRejectBlock) reject
-  )
+RCT_EXTERN_METHOD(resolvePromise: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
 
-RCT_EXTERN_METHOD(rejectPromise:
-    (RCTPromiseResolveBlock) resolve
-    rejecter: (RCTPromiseRejectBlock) reject
-  )
-  
+RCT_EXTERN_METHOD(rejectPromise: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+
 + (BOOL)requiresMainQueueSetup
 {
-  return NO;
+    return NO;
 }
 
 @end
--- ios/NativeModulesPractice.swift
+++ ios/NativeModulesPractice.swift
@@ -1,52 +1,52 @@
 @objc(NativeModulesPractice)
 class NativeModulesPractice: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
 
-  
-  @objc func plus(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a+b)
-  }
 
-  @objc func simpleMethodReturns(
-    _ callback: RCTResponseSenderBlock
-  ) {
-    callback(["CustomMethods.simpleMethodReturns()"])
-  }
- 
-  @objc func simpleMethodWithParams(
-    _ param: String,
-    callback: RCTResponseSenderBlock
-  ) {
-    callback(["CustomMethods.simpleMethodWithParams('\(param)')"])
-  }
+    @objc func plus(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a+b)
+    }
 
-  @objc func simpleMethodWithObj(
-    _ param: [String: Any],
-    callback: RCTResponseSenderBlock
-  ) {
-    callback(["CustomMethods.simpleMethodWithParams('\(param)')"])
-  }
+    @objc func simpleMethodReturns(
+        _ callback: RCTResponseSenderBlock
+    ) {
+        callback(["CustomMethods.simpleMethodReturns()"])
+    }
 
-  @objc func resolvePromise(
-    _ resolve: RCTPromiseResolveBlock,
-    rejecter reject: RCTPromiseRejectBlock
-  ) -> Void {
-    resolve("CustomMethods.resolvePromise()")
-  }
+    @objc func simpleMethodWithParams(
+        _ param: String,
+        callback: RCTResponseSenderBlock
+    ) {
+        callback(["CustomMethods.simpleMethodWithParams('\(param)')"])
+    }
 
-  @objc func rejectPromise(
-    _ resolve: RCTPromiseResolveBlock,
-    rejecter reject: RCTPromiseRejectBlock
-  ) -> Void {
-    reject("0", "CustomMethods.rejectPromise()", nil)
-  }
+    @objc func simpleMethodWithObj(
+        _ param: [String: Any],
+        callback: RCTResponseSenderBlock
+    ) {
+        callback(["CustomMethods.simpleMethodWithParams('\(param)')"])
+    }
 
-  @objc func constantsToExport() -> [String: Any]! {
-   return ["someKey": "someValue"]
- }
+    @objc func resolvePromise(
+        _ resolve: RCTPromiseResolveBlock,
+        rejecter reject: RCTPromiseRejectBlock
+    ) -> Void {
+        resolve("CustomMethods.resolvePromise()")
+    }
+
+    @objc func rejectPromise(
+        _ resolve: RCTPromiseResolveBlock,
+        rejecter reject: RCTPromiseRejectBlock
+    ) -> Void {
+        reject("0", "CustomMethods.rejectPromise()", nil)
+    }
+
+    @objc func constantsToExport() -> [String: Any]! {
+        return ["someKey": "someValue"]
+    }
 
 }
